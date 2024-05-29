7dac6b720e3c34fb17f17238ad4d4a95984cca12
William Arnold <will748@gmail.com>
2024-05-29 16:43:53 +0900

    More quantization script improvements

--- tools/quantize.py
+++ tools/quantize.py
@@ -1,9 +1,9 @@
 from awq import AutoAWQForCausalLM
-from awq.evaluation import eval_utils
+from awq.models.llama import SplitConfig
+from filelock import FileLock
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaConfig, LlamaForCausalLM
 import json
-import lm_eval
 from pathlib import Path
 import torch.nn.functional as F
 from datasets import load_dataset
@@ -13,48 +13,82 @@ import subprocess
 import torch
 from torch import nn
 
-def evaluate_perplexity(model, tokenizer):
-    model = model.to("cuda")
-    assert model.device.type == "cuda"
+cached_dataset = None
+def get_cached_dataset():
+    global cached_dataset
+    if cached_dataset is None:
+        cached_dataset = load_dataset("mli-will/slimpj-val", split="validation")
+        print("Loaded dataset with size", len(cached_dataset))
+    return cached_dataset
 
-    def _perplexity(nlls, n_samples, seqlen):
-        return torch.exp(torch.stack(nlls).sum() / (n_samples * seqlen))
+def get_data_subset(split: str, tokenizer):
+    if split == "calib":
+        data = get_cached_dataset().select(range(1000))
+    elif split == "valid":
+        data = get_cached_dataset().select(range(1000, 2000))
+    text = tokenizer.eos_token.join(data["text"])
+    text_bytes = len(text.encode("utf-8"))
+    data = tokenizer(text, return_tensors="pt").input_ids
+    del text
+    text_tokens = data.numel()
+    print(f"Split {split} has {text_tokens / 1e6:.2f}m tokens")
+    return data, text_tokens / text_bytes
 
-    # load and prepare dataset
-    data = load_dataset("mit-han-lab/pile-val-backup", split="validation")
-    data = data.select(range(1000, 2000))
-    data = tokenizer(tokenizer.eos_token.join(data["text"]), return_tensors="pt")
-    data = data.input_ids.to(model.device)
-    print("Got data shape", data.shape)
+# do this in a separate method so the closure copies layer_idx
+def make_act_stats_hook(layer_idx, act_stats, act_fn):
+    def hook(module, input, output):
+        per_channel = output.reshape(-1, output.shape[-1])
+        act_stats["act_pcts"][layer_idx] += (per_channel > 0).sum(dim=0).to(torch.int32)
+        act_stats["act_magn"][layer_idx] += act_fn(per_channel).abs().sum(dim=0)
+    return hook
 
-    seqlen = 2048
-    batch_size = 8
+def get_act_stats(model: LlamaForCausalLM, tokenizer):
+    cfg: LlamaConfig = model.config
+    act_stats = {
+        "act_pcts": torch.zeros((cfg.num_hidden_layers, cfg.intermediate_size), dtype=torch.int32, device=model.device),
+        "act_magn": torch.zeros((cfg.num_hidden_layers, cfg.intermediate_size), dtype=torch.float32, device=model.device)
+    }
+    hooks = []
+    for i, layer in enumerate(model.model.layers):
+        hk = make_act_stats_hook(i, act_stats, layer.mlp.act_fn)
+        hooks.append(layer.mlp.gate_proj.register_forward_hook(hk))
+    data, _tokens_per_byte = get_data_subset("calib", tokenizer)
+    _nll, n_tokens = eval_on_dataset(model, data) 
+    for k, v in list(act_stats.items()):
+        act_stats[k] = v.cpu().float() / n_tokens
+    for hook in hooks:
+        hook.remove()
+    return act_stats
+
+def eval_on_dataset(model, data, batch_size=8):
+    seqlen = model.config.max_position_embeddings
     model = model.eval()
     n_samples = data.numel() // (seqlen * batch_size)
-    data = data[0, :n_samples * seqlen * batch_size].view(n_samples, batch_size, seqlen)
+    n_tokens = n_samples * seqlen * batch_size
+    data = data[0, :n_tokens].view(n_samples, batch_size, seqlen)
 
     nlls = []
-
-    with tqdm(range(n_samples), desc="Perplexity -") as progress_bar:
-        for i in progress_bar:
-            batch = data[i].to(model.device)
-            with torch.inference_mode():
+    with torch.inference_mode():
+        with tqdm(range(n_samples), desc="Perplexity -") as progress_bar:
+            for i in progress_bar:
+                batch = data[i].to(model.device)
                 logits = model(batch).logits
-            shift_logits = logits[:, :-1, :].contiguous().float()
-            shift_labels = batch[:, 1:].contiguous()
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(
-                shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
-            )
-            neg_log_likelihood = loss.float() * seqlen
-            nlls.append(neg_log_likelihood)
+                shift_logits = logits[:, :-1, :].contiguous().float()
+                shift_labels = batch[:, 1:].contiguous()
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(
+                    shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+                )
+                nlls.append(loss)
 
-            curr_ppl = _perplexity(nlls, i + 1, seqlen)
-            progress_bar.set_description(f"Perplexity {curr_ppl:.3f}")
+                progress_bar.set_description(f"Perplexity {torch.stack(nlls).mean().exp():.3f}")
+    nll = torch.stack(nlls).mean()
+    return nll, n_tokens
 
-    ppl = _perplexity(nlls, n_samples, seqlen)
-
-    return ppl.item()
+def get_valid_eval_dict(model, tokenizer):
+    dset, tokens_per_byte = get_data_subset("valid", tokenizer)
+    nll, _n_tokens = eval_on_dataset(model, dset)
+    return {"eval-ppl": torch.exp(nll).item(), "eval-bpb": nll.item() * tokens_per_byte}
 
 def maybe_convert_to_hf(model_path: Path, tokenizer: str) -> Path:
     assert model_path.exists(), f"Model path {model_path} does not exist"
@@ -72,14 +106,17 @@ def maybe_convert_to_hf(model_path: Path, tokenizer: str) -> Path:
     assert (model_path / "model.safetensors").exists(), f"Model {model_path} is not a hf model"
     return model_path
 
-def write_result(out_file: Path, model_path: Path, model_cfg: LlamaConfig, results: dict):
-    with out_file.open("a") as f:
-        line = json.dumps({
-            "model": model_path.name,
-            "model_cfg": model_cfg.to_dict(),
-            **results
-        })
-        f.write(line + "\n")
+def write_result(args, model_path: Path, model_cfg: LlamaConfig, results: dict):
+    out_file = Path(args.out_file)
+    with FileLock(out_file.with_suffix(".lock")):
+        with out_file.open("a") as f:
+            line = json.dumps({
+                "model": model_path.name,
+                "model_cfg": model_cfg.to_dict(),
+                "args": vars(args),
+                **results
+            })
+            f.write(line + "\n")
 
 def main(args):
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -92,52 +129,66 @@ def main(args):
         print("Loading base model")
         base_model = AutoModelForCausalLM.from_pretrained(model_path).to(device).to(torch.bfloat16)
         print("Evaluating base model")
-        ppl = evaluate_perplexity(base_model, tokenizer)
-        res = {"wikitext": ppl}
-        #res = lm_eval.simple_evaluate(model=base_model, tasks=args.tasks, device=device)
-        write_result(args.out_file, model_path, base_model.config, res)
+        res = get_valid_eval_dict(base_model, tokenizer)
+        write_result(args, model_path, base_model.config, res)
         del base_model
 
 
-    quant_path = model_path / args.quantized_subdir
-
-    if not quant_path.exists():
+    res = {}
+    if args.quant_load is not None:
+        quant_path = Path(args.quant_load)
+        assert quant_path.exists(), f"Quantized model {args.quant_load} does not exist"
+        model = AutoAWQForCausalLM.from_quantized(str(quant_path), "model.safetensors", fuse_layers=False)
+        res["quant_counts"] = [model.config.intermediate_size for i in range(model.config.num_hidden_layers)]
+        res["non_q_counts"] = [0 for i in range(model.config.num_hidden_layers)]
+    else:
         # Load model
         print("Quantizing")
         model = AutoAWQForCausalLM.from_pretrained(
             model_path, "model.safetensors", 
         )
-        acts_path = Path(model_path) / "acts.npy"
-        if acts_path.exists():
-            acts = torch.from_numpy(np.load(str(acts_path)))
-            model.split_mlp(acts, 0.99)
+        if args.split_type is not None:
+            split_cfg = SplitConfig(
+                random_cols = args.split_type == "rand",
+                top_gt0_cols = args.split_type == "top",
+                bottom_gt0_cols = args.split_type == "bottom",
+                n_split_constant = args.split_constant,
+                n_split_top_gt0_thresh = args.split_top,
+                n_split_bottom_gt0_thresh = args.split_bottom,
+            )
+            acts_path = Path(model_path) / "act_stats.pt"
+            acts = None
+            if acts_path.exists():
+                print("Loaded activation stats from", acts_path)
+                acts = torch.load(acts_path)
+            else:
+                print("Getting activation stats")
+                acts = get_act_stats(model.to("cuda"), tokenizer)
+                torch.save(acts, str(acts_path))
+            res["quant_counts"], res["non_q_counts"] = model.split_mlp(split_cfg, act_pcts=acts["act_pcts"], act_magn=acts["act_magn"])
 
-        #print("Pre quant post split eval")
-        #evaluate_perplexity(model.model, tokenizer)
         if model.config.intermediate_size % args.group_size != 0:
             args.group_size //= 2
             print(f"Group size is not a factor of hidden size. Using group size {args.group_size}")
             assert model.config.hidden_size % args.group_size == 0
         # Quantize
         quant_config = { "zero_point": True, "q_group_size": args.group_size, "w_bit": 4, "version": "GEMM" }
-        model.quantize(tokenizer, quant_config=quant_config)
+        model.quantize(tokenizer, quant_config=quant_config, calib_data="mli-will/slimpj-val", split="validation")
 
-        print("Saving quantized model")
         # Save quantized model
-        model.save_quantized(str(quant_path))
-        tokenizer.save_pretrained(str(quant_path))
-        print(f'Model is quantized and saved at "{quant_path}"')
+        if args.quant_save is not None:
+            print("Saving quantized model")
+            quant_path = Path(args.quant_save)
+            model.save_quantized(str(quant_path))
+            tokenizer.save_pretrained(str(quant_path))
+            print(f'Model is quantized and saved at "{quant_path}"')
         model = model.model
-    else:
-        model = AutoAWQForCausalLM.from_quantized(str(quant_path), "model.safetensors", fuse_layers=False)
 
     print("Evaluating quantized model")
     # Evaluate
-    model = model.to(device)
-    ppl = evaluate_perplexity(model, tokenizer)
-    res = {"wikitext": ppl}
-    #res = lm_eval.simple_evaluate(model=model, tasks=args.tasks, out_file=args.out_file)
-    write_result(args.out_file, quant_path, model.config, res)
+    model = model.to(device) # sometimes this gets loaded on CPU
+    res |= get_valid_eval_dict(model, tokenizer)
+    write_result(args, model_path, model.config, res)
 
 
 if __name__ == "__main__":
@@ -145,10 +196,16 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, help="Path to the base model", required=True)
     parser.add_argument("--tokenizer", type=str, help="Path to the tokenizer", default="EleutherAI/pythia-70m")
     parser.add_argument("--group-size", type=int, help="Group size for quantization", default=128)
-    parser.add_argument("--quantized-subdir", type=str, help="Subdirectory to save quantized model", default="quant_v0")
-    parser.add_argument("--tasks", type=str, nargs="+", help="Tasks to evaluate on", default=[])
+    parser.add_argument("--quant-load", type=str, help="Directory to load the quantized model", default=None)
+    parser.add_argument("--quant-save", type=str, help="Directory to save the quantized model", default=None)
     parser.add_argument("--out-file", type=str, help="Output file to save results", default="results.jsonl")
     parser.add_argument("--eval-base", action="store_true", help="Evaluate base model")
+
+    parser.add_argument("--split-type", type=str, choices=["rand", "top", "bottom"], default=None)
+    parser.add_argument("--split-constant", type=int, default=None)
+    parser.add_argument("--split-top", type=float, default=None)
+    parser.add_argument("--split-bottom", type=float, default=None)
+
     args = parser.parse_args()
-    args.out_file = Path(args.out_file)
+    assert not args.quant_save and args.split_type, "Can't save split model yet"
     main(args)
